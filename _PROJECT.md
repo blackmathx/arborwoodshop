48c8c7e7f88acecf23dbaabe94e2cacd2e64f046
bo <jepayton12013@gmail.com>
2024-09-14 18:25:18 -0400

    Tailwind updates

--- _PROJECT.md
+++ _PROJECT.md
@@ -5,5 +5,5 @@ Project
    manually with any changes at release time.
 3. Tailwind https://tailwindcss.com/. Tailwind development has a script in package.json. Run "npm run watch" to watch 
    for changes and build the css file. Then start the spring boot project.
-4. The release document has the steps to release to AWS EC2. This is in the project folder on the Black Math data drive.
+4. The release document has the steps to release to AWS EC2. This is in the project folder
   
--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["../resources/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    corePlugins: {
+      preflight: false,
+    },
+    fontFamily: {
+      sans: ['arial', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+            'verylightblue': '#F5F9FE',
+            'mossgreen': '#919653',
+            'bluegray': '#6B5B9A',
+            'blueyonder': '#537796',
+            'pine': '#5B9A8A',
+            'link': '#4d4de7',
+            'linkvisited': '#912faf',
+            'linkactive': '#FF0000',
+
+            'tetrad': '#539679',
+
+
+        }
+    },
   },
   plugins: [],
+
 }
 
